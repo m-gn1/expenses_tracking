d7665c01b0f1fdb950543f18ec83f81a03b4445b
Marie Genin <gninmarie@gmail.com>
2025-07-13 19:21:06 +0100

    test dossier locaux

--- pages/0_NC local streamlit.py
+++ pages/0_NC local streamlit.py
@@ -9,12 +9,12 @@ client = connect_to_nextcloud()
 if client: 
 # 📁 Chemins
     remote_folder = "/test_streamlit"
-    local_folder = "./data"  # ou "./cache", etc.
+    local_folder = "./cache"  # ou "./cache", etc.
 
     # 🔄 Synchronisation
     sync_from_nextcloud_to_server(client, remote_folder, local_folder)
 
     # 🧾 Tu peux ensuite lire tes fichiers localement
     import pandas as pd
-    df = pd.read_csv("./data/expenses_data.csv")
+    df = pd.read_csv("./cache/expenses_data.csv")
     st.dataframe(df)
