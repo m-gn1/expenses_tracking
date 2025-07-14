4e0073c42b728315199c0b44474551da03bb1f62
Marie Genin <gninmarie@gmail.com>
2025-07-14 16:55:28 +0100

    debug 4

--- pages/0_1_5 DEBUG SAVE.py
+++ pages/0_1_5 DEBUG SAVE.py
@@ -1,21 +1,3 @@
-import pandas as pd
-import os
-import streamlit as st
-
-REMOTE_IMPORTED_FOLDER = "data/imported_data/"
-
-df = pd.DataFrame({"col1": [1, 2], "col2": ["a", "b"]})
-st.dataframe(df)
-output_path_imported_cache = os.path.join(".cache/", REMOTE_IMPORTED_FOLDER, "data.csv")
-df.to_csv(output_path_imported_cache, index=False)
-st.success("csv dans cache")
-
-
-df_test = pd.read_csv(output_path_imported_cache)
-st.dataframe(df_test)
-
-
-
 import os
 import pandas as pd
 import streamlit as st
