5e746edee9252e661c8576e010fa376a7d15d00a
Marie Genin <gninmarie@gmail.com>
2025-08-03 10:24:44 +0100

    Attribute_users_NC_FINALISED

--- pages/0_4_Attribute users.py
+++ pages/0_4_Attribute users.py
@@ -1,6 +1,3 @@
-import streamlit as st
-st.title("TODO")
-st.write("récuperr fichier de nextcloud 'importer', make sure cache importer is empty, importer les fichiers dans le cache, et les traiter un par un. ")
 
 import os
 import streamlit as st
---
+++ pages/0_5_Update_categories.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("TODO")
+st.write("recuperer expenses data de remote a local, reprendre le meme code ")
